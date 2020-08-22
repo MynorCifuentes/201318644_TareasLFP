30cbebc60595457fc523e4be772c1c4ee53767c8
Mynor Cifuentes <mynorcifuentes@ieee.org>
2020-08-21 22:15:03 -0600

    archivo python listo

---
+++ Tarea2/estudiantes.py
@@ -0,0 +1,14 @@
+import json
+
+def readJson():
+    miArchivo = open('estudiantes.json')
+    data = json.load(miArchivo)
+    miArchivo.close()
+    print(data)
+    return data
+
+dict=readJson()
+for palabras in dict:
+    print(palabras)
+
+
