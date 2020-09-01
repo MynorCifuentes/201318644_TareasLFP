05d9b19493aba2ea8be0313d49e779f5199699db
Mynor Cifuentes <mynorcifuentes@ieee.org>
2020-09-01 15:56:03 -0600

    Tarea 3 completa

--- Tarea2/main.py
+++ Tarea2/main.py
@@ -1,2 +1,10 @@
 import os
-os.system('archivo1.py')
+# os.system('archivo1.py')
+print("[1] Archivo1 JSON")
+print("[2] Archivo2 CSV")
+print("[3] Archivo3 XML")
+opcion = input('Elija que archivo quiere ejecutar')
+
+
+   
+    # os.system('archivo1.py')
